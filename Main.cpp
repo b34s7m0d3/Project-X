9884924d73c81f3f6c5ba8ac263fdb1e938f8bd4
Peter <minimedve@gmail.com>
2017-02-12 17:08:20 +0100

    Integrating Libtcod with building path

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
+#include "libtcod.hpp"
 
-using namespace std;
-
-int main(){
-
-cout << "Smeditem"<< endl;
-
-return 0;
+int main() {
+    TCODConsole::initRoot(80,50,"test",false);
+    while (! TCODConsole::isWindowClosed()) {
+        TCODConsole::root->clear();
+ //       TCODConsole::root->printCenter(40,25,TCOD_BKGND_NONE,"Hello world!");
+        TCODConsole::flush();
+        TCODConsole::checkForKeypress();
+    }
 }
