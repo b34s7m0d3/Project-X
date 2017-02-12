4d44107878b46b055f1f375731f97646fd6c6cb9
Peter <minimedve@gmail.com>
2017-02-12 19:50:21 +0100

    First few lines
    First console display

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,21 @@
 #include "libtcod.hpp"
 
 int main() {
-    TCODConsole::initRoot(80,50,"test",false);
-    while (! TCODConsole::isWindowClosed()) {
-        TCODConsole::root->clear();
- //       TCODConsole::root->printCenter(40,25,TCOD_BKGND_NONE,"Hello world!");
-        TCODConsole::flush();
-        TCODConsole::checkForKeypress();
-    }
+   int playerx=40,playery=25;
+   TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
+   while ( !TCODConsole::isWindowClosed() ) {
+       TCOD_key_t key;
+       TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
+       switch(key.vk) {
+           case TCODK_UP : playery--; break;
+           case TCODK_DOWN : playery++; break;
+           case TCODK_LEFT : playerx--; break;
+           case TCODK_RIGHT : playerx++; break;
+           default:break;
+       }
+       TCODConsole::root->clear();
+       TCODConsole::root->putChar(playerx,playery,'@');
+       TCODConsole::flush();
+   }
+   return 0;
 }
