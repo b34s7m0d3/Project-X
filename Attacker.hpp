e8db4e90a473fc045aa01a7a1c1279919ceabc45
b34s7m0d3 <peter@petermehlmann.com>
2018-04-24 18:56:37 +0200

    Removing old files

--- Attacker.hpp
+++
@@ -1,9 +0,0 @@
-class Attacker : public Persistent {
-public :
-	float power; // hit points given
-
-	Attacker(float power);
-	void attack(Actor *owner, Actor *target);
-	void load (TCODZip &zip);
-	void save (TCODZip &zip);
-};
